cf0dc2846108b3eb0e40e33b55d98b3dc4c3521e
demji001 <demjittjum@gmail.com>
2025-07-02 07:57:00 -0400

    Added more files
    It wasn't pushing

---
+++ script.js
@@ -0,0 +1,14 @@
+/*let circleTopLeft = document.querySelector(".circle-top-left");
+let circleTopRight = document.querySelector(".circle-top-right");
+let circleBottomLeft = document.querySelector(".circle-bottom-left");
+let circleBottomRight = document.querySelector(".circle-bottom-right");
+
+let githubLarge = document.querySelector(".github-large");
+let linkedinLarge = document.querySelector(".linkedin-large");
+
+let github = document.querySelector(".github");
+let linkedin = document.querySelector(".linkedin");
+
+circleTopLeft.addEventListener("click", function() {
+
+});
